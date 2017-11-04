899120d9b3a5c79e0e49bba0d36c0333ae0211fe
Ashwin Rao <ashwin.rao@gmail.com>
2017-11-04 16:02:55 -0700

    few applications

--- wiki_word2vec_similar.py
+++ wiki_word2vec_similar.py
@@ -1,30 +1,49 @@
 import pandas as pd
 import numpy as np
 
-subset_words = -1
-word = "assuage"
-print "Given word is %s" % word
-num_nearest_words = 20
-filename = "/Users/z001xdc/Downloads/wiki_w2v.vec"
-df = pd.io.parsers.read_csv(
-    filename,
-    sep=' ',
-    engine='c',
-    skiprows=1,
-    index_col=0,
-    header=None,
-    nrows=(subset_words if subset_words > 0 else None)
-)
-words = list(df.index)
-word_index = words.index(word)
-vecs = df.as_matrix()
-vec_norms = np.linalg.norm(vecs, axis=1)
 
-word_vec = vecs[word_index]
-word_vec_norm = np.linalg.norm(word_vec)
-# cosines = vecs.dot(vecs.T) / np.outer(vec_norms, vec_norms)
-cosines = vecs.dot(word_vec) / vec_norms / word_vec_norm
-nearest_words = [words[i] for i in
-                 cosines.argsort()[-num_nearest_words:]][::-1]
-print "%d nearest-by-context words are:" % num_nearest_words
-print nearest_words
+def get_vec_from_word(df, word):
+    return df.loc[word]
+
+
+def get_nearest_words(df, vec, num_nearest_words):
+    vecs = df.as_matrix()
+    vec_norms = np.linalg.norm(vecs, axis=1)
+    vec_norm = np.linalg.norm(vec)
+    cosines = vecs.dot(vec) / vec_norms / vec_norm
+    nearest_word_indices = cosines.argsort()[-num_nearest_words:][::-1]
+    return df.index[nearest_word_indices]
+
+if __name__ == "__main__":
+    subset_words = -1
+    num_nearest_words = 20
+
+    filename = "/Users/z001xdc/Downloads/wiki_w2v.vec"
+    df = pd.io.parsers.read_csv(
+        filename,
+        sep=' ',
+        engine='c',
+        skiprows=1,
+        index_col=0,
+        header=None,
+        nrows=(subset_words if subset_words > 0 else None)
+    )
+
+    # word = "assuage"
+    # print "Given word is %s" % word
+    # x_vec = get_vec_from_word(df, word)
+
+    # ireland_vec = get_vec_from_word(df, "ireland")
+    # dublin_vec = get_vec_from_word(df, "dublin")
+    # moscow_vec = get_vec_from_word(df, "moscow")
+    # x_vec = ireland_vec - dublin_vec + moscow_vec
+
+    country = "india"
+    entity = "language"
+    country_vec = get_vec_from_word(df, country)
+    entity_vec = get_vec_from_word(df, entity)
+    x_vec = country_vec + entity_vec
+
+    nearest_words = get_nearest_words(df, x_vec, num_nearest_words)
+    print "%d nearest-by-context words are:" % num_nearest_words
+    print nearest_words
